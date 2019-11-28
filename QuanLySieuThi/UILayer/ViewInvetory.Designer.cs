08e427a300a67ab07d09a8165d10c135b69d41c0
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-28 20:32:38 +0700

    i dont know
    Design.cs of ViewInventory

--- QuanLySieuThi/UILayer/ViewInvetory.Designer.cs
+++ QuanLySieuThi/UILayer/ViewInvetory.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ViewInvetory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 348);
+            this.Name = "ViewInvetory";
             this.Text = "ViewInvetory";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
