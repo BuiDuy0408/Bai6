c07cb5cd8b668451e7388546b8809417801dc0a7
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-28 13:46:55 +0700

    thanh-add load form

--- QuanLySieuThi/UILayer/UserControlForm.cs
+++ QuanLySieuThi/UILayer/UserControlForm.cs
@@ -20,5 +20,20 @@ namespace UILayer
         {
             InitializeComponent();
         }
+        private void UserControlForm_Load(object sender, EventArgs e)
+        {
+            dt = _employeeBusiness.GetEmployeeDetail(name);
+            textBox1.Text = dt.Rows[0]["Employee_Name"].ToString();
+            textBox2.Text = dt.Rows[0]["Employee_AGe"].ToString();
+            textBox3.Text = dt.Rows[0]["Employee_Gender"].ToString();
+            textBox4.Text = dt.Rows[0]["Employee_MaritalStatus"].ToString();
+            textBox5.Text = dt.Rows[0]["Employee_NID"].ToString();
+            textBox6.Text = dt.Rows[0]["Employee_JobPosition"].ToString();
+            textBox7.Text = dt.Rows[0]["Employee_StartDate"].ToString();
+            textBox8.Text = dt.Rows[0]["Employee_EndDate"].ToString();
+            textBox9.Text = dt.Rows[0]["Employee_JobDuration"].ToString();
+            richTextBox1.Text = dt.Rows[0]["Employee_Address"].ToString();
+
+        }
     }
 }
