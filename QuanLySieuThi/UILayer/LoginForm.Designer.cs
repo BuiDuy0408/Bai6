0df92255184976b93fc153279a8ccecaa60d11d7
truong19961405 <54049414+truong19961405@users.noreply.github.com>
2019-11-28 21:10:23 +0700

    xtruong
    xtruong_form_posystem

--- QuanLySieuThi/UILayer/LoginForm.Designer.cs
+++ QuanLySieuThi/UILayer/LoginForm.Designer.cs
@@ -29,36 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.labelSuperShopName = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pBoxPass = new System.Windows.Forms.PictureBox();
-            this.pBoxUser = new System.Windows.Forms.PictureBox();
-            this.tbPassword = new System.Windows.Forms.TextBox();
             this.tbUserID = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.pBoxUser = new System.Windows.Forms.PictureBox();
+            this.pBoxPass = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.panelLogin = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPass)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPass)).BeginInit();
             this.panelLogin.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // labelSuperShopName
-            // 
-            this.labelSuperShopName.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelSuperShopName.BackColor = System.Drawing.Color.Transparent;
-            this.labelSuperShopName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.labelSuperShopName.Font = new System.Drawing.Font("Monotype Corsiva", 45F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSuperShopName.ForeColor = System.Drawing.Color.Transparent;
-            this.labelSuperShopName.Location = new System.Drawing.Point(181, 11);
-            this.labelSuperShopName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelSuperShopName.Name = "labelSuperShopName";
-            this.labelSuperShopName.Size = new System.Drawing.Size(684, 144);
-            this.labelSuperShopName.TabIndex = 2;
-            this.labelSuperShopName.Text = "Friend Super Shop";
-            this.labelSuperShopName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel1
             // 
@@ -78,27 +62,17 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(439, 91);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // pBoxPass
+            // tbUserID
             // 
-            this.pBoxPass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pBoxPass.BackgroundImage")));
-            this.pBoxPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBoxPass.Location = new System.Drawing.Point(4, 49);
-            this.pBoxPass.Margin = new System.Windows.Forms.Padding(4);
-            this.pBoxPass.Name = "pBoxPass";
-            this.pBoxPass.Size = new System.Drawing.Size(52, 38);
-            this.pBoxPass.TabIndex = 3;
-            this.pBoxPass.TabStop = false;
-            // 
-            // pBoxUser
-            // 
-            this.pBoxUser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pBoxUser.BackgroundImage")));
-            this.pBoxUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pBoxUser.Location = new System.Drawing.Point(4, 4);
-            this.pBoxUser.Margin = new System.Windows.Forms.Padding(4);
-            this.pBoxUser.Name = "pBoxUser";
-            this.pBoxUser.Size = new System.Drawing.Size(52, 37);
-            this.pBoxUser.TabIndex = 2;
-            this.pBoxUser.TabStop = false;
+            this.tbUserID.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbUserID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserID.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tbUserID.Location = new System.Drawing.Point(65, 4);
+            this.tbUserID.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUserID.Name = "tbUserID";
+            this.tbUserID.Size = new System.Drawing.Size(370, 30);
+            this.tbUserID.TabIndex = 0;
             // 
             // tbPassword
             // 
@@ -112,17 +86,27 @@
             this.tbPassword.TabIndex = 1;
             this.tbPassword.UseSystemPasswordChar = true;
             // 
-            // tbUserID
+            // pBoxUser
             // 
-            this.tbUserID.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.tbUserID.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbUserID.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.tbUserID.Location = new System.Drawing.Point(65, 4);
-            this.tbUserID.Margin = new System.Windows.Forms.Padding(4);
-            this.tbUserID.Name = "tbUserID";
-            this.tbUserID.Size = new System.Drawing.Size(370, 30);
-            this.tbUserID.TabIndex = 0;
+            this.pBoxUser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pBoxUser.BackgroundImage")));
+            this.pBoxUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBoxUser.Location = new System.Drawing.Point(4, 4);
+            this.pBoxUser.Margin = new System.Windows.Forms.Padding(4);
+            this.pBoxUser.Name = "pBoxUser";
+            this.pBoxUser.Size = new System.Drawing.Size(52, 37);
+            this.pBoxUser.TabIndex = 2;
+            this.pBoxUser.TabStop = false;
+            // 
+            // pBoxPass
+            // 
+            this.pBoxPass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pBoxPass.BackgroundImage")));
+            this.pBoxPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBoxPass.Location = new System.Drawing.Point(4, 49);
+            this.pBoxPass.Margin = new System.Windows.Forms.Padding(4);
+            this.pBoxPass.Name = "pBoxPass";
+            this.pBoxPass.Size = new System.Drawing.Size(52, 38);
+            this.pBoxPass.TabIndex = 3;
+            this.pBoxPass.TabStop = false;
             // 
             // label1
             // 
@@ -186,23 +170,20 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1027, 464);
             this.Controls.Add(this.panelLogin);
-            this.Controls.Add(this.labelSuperShopName);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxPass)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPass)).EndInit();
             this.panelLogin.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelSuperShopName;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox tbUserID;
         private System.Windows.Forms.TextBox tbPassword;
