7f338f30a938785189c63d46c776a2d3247ff8f3
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-28 21:22:27 +0700

    Update Store.Designer.cs
    edit UI

--- QuanLySieuThi/UILayer/Store.Designer.cs
+++ QuanLySieuThi/UILayer/Store.Designer.cs
@@ -28,31 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelSelect = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelSelect = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // labelSelect
-            // 
-            this.labelSelect.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSelect.Location = new System.Drawing.Point(14, 9);
-            this.labelSelect.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelSelect.Name = "labelSelect";
-            this.labelSelect.Size = new System.Drawing.Size(175, 38);
-            this.labelSelect.TabIndex = 3;
-            this.labelSelect.Text = "Select Catagory";
-            this.labelSelect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(197, 13);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(316, 32);
-            this.comboBox1.TabIndex = 4;
             // 
             // button1
             // 
@@ -65,11 +44,32 @@
             this.button1.Text = "Show";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(197, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(316, 32);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // labelSelect
+            // 
+            this.labelSelect.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelect.Location = new System.Drawing.Point(14, 9);
+            this.labelSelect.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSelect.Name = "labelSelect";
+            this.labelSelect.Size = new System.Drawing.Size(175, 38);
+            this.labelSelect.TabIndex = 3;
+            this.labelSelect.Text = "Select Catagory";
+            this.labelSelect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Store
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(895, 467);
+            this.ClientSize = new System.Drawing.Size(801, 450);
             this.Controls.Add(this.labelSelect);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -81,8 +81,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label labelSelect;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelSelect;
     }
 }
